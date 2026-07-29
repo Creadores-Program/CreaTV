c0c65685719658f1f369c65e46bc160eb848980b
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-07-29 14:00:25 -0600

    Refactor ChatActivity to load local chat HTML
    Removed custom tab browser logic and updated URL loading to use a local HTML file.

--- app/src/main/java/org/CreadoresProgram/CreaTv/ChatActivity.java
+++ app/src/main/java/org/CreadoresProgram/CreaTv/ChatActivity.java
@@ -6,13 +6,11 @@ import android.os.Bundle;
 import android.webkit.WebView;
 import android.net.Uri;
 import android.graphics.Color;
-import androidx.browser.customtabs.CustomTabsIntent;
 
 import org.CreadoresProgram.CreaTv.utils.Util;
 
 public class ChatActivity extends Activity {
     private WebView webView;
-    private boolean isTabBrowser = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -21,20 +19,10 @@ public class ChatActivity extends Activity {
             finish();
             return;
         }
-        String url = "https://nightdev.com/hosted/obschat/?channel="+getIntent().getExtras().getString(Util.CREATORNAME)+"&fade=false";
-        if(Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT){
-            CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
-            builder.setShowTitle(false);
-            builder.setToolbarColor(Color.BLACK);
-            CustomTabsIntent customTabsIntent = builder.build();
-            this.isTabBrowser = true;
-            customTabsIntent.launchUrl(this, Uri.parse(url+"&theme=dark"));
-            return;
-        }
         setContentView(R.layout.layout_main);
         this.webView = (WebView) findViewById(R.id.webview);
         Util.configWebView(this.webView, this);
-        webView.loadUrl(url);
+        webView.loadUrl("file:///android_asset/chat/chat.html?channel="+getIntent().getExtras().getString(Util.CREATORNAME));
     }
     @Override
     protected void onPause(){
@@ -47,9 +35,6 @@ public class ChatActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
-        if(this.isTabBrowser){
-            finish();
-        }
         if(this.webView != null){
             webView.onResume();
             webView.resumeTimers();
