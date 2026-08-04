e60902ffad995b20b3969dbf5c5cca9c4b67aa32
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-03 20:09:50 -0600

    Add YTPlayerActivity for YouTube streaming

---
+++ app/src/main/java/org/CreadoresProgram/CreaTv/YTPlayerActivity.java
@@ -0,0 +1,48 @@
+package org.CreadoresProgram.CreaTv;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import org.CreadoresProgram.CreaTv.utils.Util;
+
+public class YTPlayerActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+        this.webView = (WebView) findViewById(R.id.webview);
+        Util.configWebView(this.webView, this);
+        webView.loadUrl("file:///android_asset/youtube/StreamYT.html");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if(this.webView != null){
+            webView.onPause();
+            webView.pauseTimers();
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(this.webView != null){
+            webView.onResume();
+            webView.resumeTimers();
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        webView.post(new Runnable(){
+            @Override
+            public void run(){
+                webView.destroy();
+                webView = null;
+            }
+        });
+        super.onDestroy();
+    }
+}
