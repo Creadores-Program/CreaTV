9503ee50630d056d645bc62b8e4a5a9df54e212d
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-03 21:16:40 -0600

    Load YouTube HTML from assets with base URL

--- app/src/main/java/org/CreadoresProgram/CreaTv/YTPlayerActivity.java
+++ app/src/main/java/org/CreadoresProgram/CreaTv/YTPlayerActivity.java
@@ -20,7 +20,8 @@ public class YTPlayerActivity extends Activity {
         setContentView(R.layout.layout_main);
         this.webView = (WebView) findViewById(R.id.webview);
         Util.configWebView(this.webView, this);
-        webView.loadUrl("file:///android_asset/youtube/StreamYT.html?v="+videoId);
+        String htmlCode = Util.readAssetAsString(getAssets(), "youtube/StreamYT.html");
+        webView.loadDataWithBaseURL("https://www.youtube-nocookie.com", htmlCode, "text/html", "UTF-8", "https://www.youtube-nocookie.com?v="+videoId);
     }
 
     @Override
