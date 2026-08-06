f6f2fdc20a70ebc9139d279fc02214fc35640525
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-06 12:16:00 -0600

    Implement full-screen mode in YTPlayerActivity
    Added full-screen mode functionality to YTPlayerActivity.

--- app/src/main/java/org/CreadoresProgram/CreaTv/YTPlayerActivity.java
+++ app/src/main/java/org/CreadoresProgram/CreaTv/YTPlayerActivity.java
@@ -1,8 +1,11 @@
 package org.CreadoresProgram.CreaTv;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.view.View;
+import android.view.Window;
 
 import org.CreadoresProgram.CreaTv.utils.Util;
 
@@ -18,6 +21,8 @@ public class YTPlayerActivity extends Activity {
             return;
         }
         setContentView(R.layout.layout_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        applyFull();
         this.webView = (WebView) findViewById(R.id.webview);
         Util.configWebView(this.webView, this);
         webView.loadUrl("https://creatv.onrender.com/youtube-player?v="+videoId);
@@ -49,5 +54,24 @@ public class YTPlayerActivity extends Activity {
             }
         });
         super.onDestroy();
+    }
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus && !isFinishing()) {
+            applyFull();
+        }
+    }
+    private void applyFull(){
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            uiOptions |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                |  View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                |  View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                |  View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        }
+        getWindow().getDecorView().setSystemUiVisibility(uiOptions);
     }
 }
