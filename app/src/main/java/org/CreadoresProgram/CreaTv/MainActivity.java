fa93ebf95d689cab56a9df3a2d21e4758a965a59
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-09-11 08:10:44 -0600

    Enhance memory management in MainActivity

--- app/src/main/java/org/CreadoresProgram/CreaTv/MainActivity.java
+++ app/src/main/java/org/CreadoresProgram/CreaTv/MainActivity.java
@@ -2,6 +2,8 @@ package org.CreadoresProgram.CreaTv;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Build;
+import android.content.ComponentCallbacks2;
 import android.webkit.WebView;
 
 import org.CreadoresProgram.CreaTv.utils.Util;
@@ -21,21 +23,61 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    protected void onPause(){
-        super.onPause();
-        if(this.webView != null){
-            webView.onPause();
-            webView.pauseTimers();
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+        if(level >= ComponentCallbacks2.TRIM_MEMORY_MODERATE){
+            uniLowMem();
         }
     }
     @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        uniLowMem();
+    }
+
+    private void uniLowMem(){
+        if (webView != null) {
+            webView.clearCache(false);
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                webView.freeMemory();
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (this.webView != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                webView.onPause();
+            } else {
+                try {
+                    WebView.class.getMethod("onPause").invoke(webView);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            webView.pauseTimers();
+        }
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
-        if(this.webView != null){
-            webView.onResume();
+        if (this.webView != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) { // API 11+
+                webView.onResume();
+            } else {
+                try {
+                    WebView.class.getMethod("onResume").invoke(webView);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
             webView.resumeTimers();
         }
     }
+
     @Override
     protected void onDestroy() {
         webView.post(new Runnable(){
