671569a3f099a22915acaa78cb6f7a56876756df
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-09-11 08:11:58 -0600

    Implement memory management in YTPlayerActivity
    Added memory management methods to handle low memory situations and modified onPause/onResume methods to support older WebView APIs.

--- app/src/main/java/org/CreadoresProgram/CreaTv/YTPlayerActivity.java
+++ app/src/main/java/org/CreadoresProgram/CreaTv/YTPlayerActivity.java
@@ -3,6 +3,7 @@ package org.CreadoresProgram.CreaTv;
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
+import android.content.ComponentCallbacks2;
 import android.webkit.WebView;
 import android.view.View;
 import android.view.Window;
@@ -41,21 +42,64 @@ public class YTPlayerActivity extends Activity {
     }
 
     @Override
-    protected void onPause(){
-        super.onPause();
-        if(this.webView != null){
-            webView.onPause();
-            webView.pauseTimers();
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+        if(level >= ComponentCallbacks2.TRIM_MEMORY_MODERATE){
+            uniLowMem();
         }
     }
     @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        uniLowMem();
+    }
+
+
+    private void uniLowMem(){
+        if (webView != null) {
+            webView.clearCache(false);
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                webView.freeMemory();
+            }
+        }
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (this.webView != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                webView.onPause();
+            } else {
+                try {
+                    WebView.class.getMethod("onPause").invoke(webView);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            webView.pauseTimers();
+        }
+    }
+
+
+    @Override
     protected void onResume() {
         super.onResume();
-        if(this.webView != null){
-            webView.onResume();
+        if (this.webView != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) { // API 11+
+                webView.onResume();
+            } else {
+                try {
+                    WebView.class.getMethod("onResume").invoke(webView);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
             webView.resumeTimers();
         }
     }
+    
     @Override
     protected void onDestroy() {
         webView.post(new Runnable(){
